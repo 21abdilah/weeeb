d0ef43ee3a933c2be944f5b7b6bae60880098f7d
21abdilah <158019652+21abdilah@users.noreply.github.com>
2025-10-02 06:29:50 +0700

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,19 +1,18 @@
 // nuxt.config.ts
 export default defineNuxtConfig({
-  ssr: false, // hanya client-side (karena pakai kamera & speech)
-  nitro: {
-    preset: 'vercel'
-  },
+  ssr: false,
+  css: [
+    '@/assets/main.css'
+  ],
   app: {
     head: {
-      title: "Gesture Detection Demo",
+      title: 'Webcam Gesture',
       meta: [
-        { name: 'viewport', content: 'width=device-width, initial-scale=1.0' }
+        { name: 'description', content: 'webcam gesture nuxt3 mediapipe' }
       ],
       script: [
-        { src: "https://cdn.jsdelivr.net/npm/@mediapipe/holistic/holistic.js" },
-        { src: "https://cdn.jsdelivr.net/npm/@mediapipe/camera_utils/camera_utils.js" },
-        { src: "https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js" }
+        { src: 'https://cdn.jsdelivr.net/npm/@mediapipe/holistic/holistic.js', defer: true },
+        { src: 'https://cdn.jsdelivr.net/npm/@mediapipe/drawing_utils/drawing_utils.js', defer: true }
       ]
     }
   }
