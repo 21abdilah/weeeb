ce92b512db19ee4d0b73a08952d16617818de7c3
21abdilah <158019652+21abdilah@users.noreply.github.com>
2025-10-05 20:01:07 +0700

    Update WebcamGesture.vue

--- components/WebcamGesture.vue
+++ components/WebcamGesture.vue
@@ -226,7 +226,7 @@ function filterHistory(handName,count){
 }
 
 /* Perkenalan gesture */
-const perkenalanGestures = {1:'HALLO EVERYONE, PERKENALKAN SAYA HILAL ABDILAH!',2:'DARI PRODI TEKNIK INFORMATIKA',3:'HOBI : BERMAIN GAME!',4:'ALASAN MASUK KARENA LOKASI YANG STRATEGIS DAN NYAMAN .',5:'Terima kasih!'}
+const perkenalanGestures = {1:'Salam, Perkenalkan Saya Hilal Abdilah!',2:'Dari prodi Teknik Informatika',3:'HOBI : Memasak!',4:'Alasan Masuk Karena Lokasi Yanh Strategis Dan Nyaman .',5:'Terima kasih!'}
 
 function detectFingerNumber(left,right){
   const now = Date.now()
